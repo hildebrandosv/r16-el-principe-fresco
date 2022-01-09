c131529fec8e56a159b91d57d725214acbe2f510
hildebrandosv <hildebrandosv@gmail.com>
2022-01-09 16:19:40 -0500

    Creación de repositorio y subida de HTML inicial

---
+++ README.md
@@ -0,0 +1,31 @@
+# Front End 7 | Academia Geek
+## Reto: El Príncipe Fresco | Work Shop | 10 jan 2022 | Timelines
+
+> ## Formador : John Florez | @JohnFlorez25
+> ## [WebPage] 
+#### [Repositorio] 
+___
+### --> ENUNCIADO:
+#### * 
+> [Link_Ejercicio] 
+### CONSIDERACIONES TÉCNICAS:
+* Debe ...
+* Usar ...
+* ...
+* **Implementar Webpack y Babel**
+
+
+## --> PANTALLAS DE EVIDENCIA:
+
+### - Envío/Entrega:
++ En Academia Geek - Front End 7:
+
+### - Código fuente:
+
+### - Resultado:
+
+___
+### Alexandra Sivira C.
+### Hildebrando Sierra V.
+### José Andrés Sánchez L.
+
---
+++ index.js
@@ -0,0 +1,27 @@
+// Definición de constantes
+const URL_PRODUCTOS = 'http://localhost:4001/tbProducts/'
+const idContenedorTarjetasProd= document.getElementById('idContenedorTarjetasProd');
+
+// Módulo PRINCIPAL del programa
+//
+// Cargar las tarjetas de los productos al cargar la página
+window.addEventListener('DOMContentLoaded', fnGetProductos())
+
+// Definición de funciones
+// Función para "Petición GET", o sea para sólo traer los datos de la tabla.
+async function fnGetProductos() {
+   const respuesta = await fetch(URL_PRODUCTOS);
+   const data = await respuesta.json();
+   data.forEach(element => {
+      idContenedorTarjetasProd.innerHTML += `
+            <div class="card m-2" style="width: 13rem;">
+               <img src="${element.imagen_url}" class="card-img-top">
+               <div class="card-body">
+                  <h5 class="card-title">${element.nombre}</h5>
+                  <p class="card-text">${element.precio}</p>
+                  <a href="#" class="btn btn-primary w-100">Agregar</a>
+               </div>
+            </div>
+         `
+   });
+}
